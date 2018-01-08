053f60ac3f0447428a3a381c66f3f0b453426e4a
Stanislav Vorobiov <sheffmail@mail.ru>
2018-01-08 11:55:38 +0300

    return orig coco stuff

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
-LD_LIBRARY_PATH=/usr/local/cuda/lib64 \
-./darknet detector test cfg/coco.data cfg/yolo.cfg scripts/backup/yolo_100.weights data/person.jpg -thresh 0.9
+#LD_LIBRARY_PATH=/usr/local/cuda/lib64 \
+#./darknet detector test cfg/coco.data cfg/yolo.cfg scripts/backup/yolo_100.weights data/dog.jpg -thresh 0.9
+./darknet detector demo cfg/coco.data cfg/yolo.cfg scripts/backup/yolo_100.weights /home/stas/Projects/1-pred/prednet/test3.avi -thresh 0.9
